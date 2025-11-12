e830a7f9bbf5a2930687baefaa19a2f69304b827
Samuel Olabode <Solabode499@gmail.com>
2025-11-12 11:17:01 -0700

    Added a simple double dice page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Chicken from './chicken'
 import Greeter from './Greeter'
 import Die from './die'
 import ListPicker from './ListPicker'
+import DoubleDice from './DoubleDice'
 
 function App() {
   return <div>
-    <ListPicker values={[1,2,2,4,4]}/>
-    {/* <Die numSides={20}/>
-    <Die numSides={30}/>
-    <Die numSides={12}/> */}
+    {/* <ListPicker values={[1,2,2,4,4]}/> */}
+    <DoubleDice />
+    <DoubleDice />
+    <DoubleDice />
   </div>
 }
 
