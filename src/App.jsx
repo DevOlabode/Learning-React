e83c2454b9f18e4600a82f207c1d51b9b571ee3c
Samuel Olabode <Solabode499@gmail.com>
2025-11-12 15:33:00 -0700

    added update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Greeter from './Greeter'
 import Die from './die'
 import ListPicker from './ListPicker'
 import DoubleDice from './DoubleDice'
+import Heading from './Heading'
 
 function App() {
   return <div>
+    <Heading color="Magenta" text="Welcome"/>
     {/* <ListPicker values={[1,2,2,4,4]}/> */}
     <DoubleDice />
     <DoubleDice />
---
+++ src/Heading.jsx
@@ -0,0 +1,3 @@
+export default function Heading({color ='Olive', text}) {
+    return <h1 style={{color : color }}>{text}</h1>
+}
